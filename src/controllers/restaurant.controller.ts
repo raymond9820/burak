221d5e3b9780ac590b1d2168db9b98c22c67f29c
Raymond raymond9820 <rashid.r1998@gmail.com>
2026-08-11 21:41:58 +0900

    feat: create restaurant member related router get and post endpoints

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -6,12 +6,13 @@ restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log(`goHome`);
     res.send("Home Page");
+    // send | json | redirect | end | render
   } catch (err) {
     console.log("ERORR, goHome: ", err);
   }
 };
 
-restaurantController.getLOgin = (req: Request, res: Response) => {
+restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log(`getLOgin`);
     res.send("login Page");
@@ -26,6 +27,23 @@ restaurantController.getSignUp = (req: Request, res: Response) => {
     res.send("signUp Page");
   } catch (err) {
     console.log("ERORR, getSignUp: ", err);
+  }
+};
+
+restaurantController.processLogin = (req: Request, res: Response) => {
+  try {
+    console.log("processlogin");
+    res.send("Done!!!!");
+  } catch (err) {
+    console.log("ERORR, processLogin: ", err);
+  }
+};
+restaurantController.processSignup = (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+    res.send("Done!!!!");
+  } catch (err) {
+    console.log("ERORR, processSignup: ", err);
   }
 };
 
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,10 +2,15 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
+//Restaurant
 routerAdmin.get("/", restaurantController.goHome);
+routerAdmin
+  .get("/login/", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
+routerAdmin
+  .get("/signup", restaurantController.getSignUp)
+  .post("/signup", restaurantController.processSignup);
 
-routerAdmin.get("/login", restaurantController.getLOgin);
-
-routerAdmin.get("/signup", restaurantController.getSignUp);
-
+//Product
+//User
 export default routerAdmin;

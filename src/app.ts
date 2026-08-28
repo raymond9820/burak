6b049c8ee6d48ac62d8c42b98addaa4fba85c8c7
Raymond raymond9820 <rashid.r1998@gmail.com>
2026-08-28 10:12:47 +0900

    feat:integrate middleware session

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,15 @@ import router from "./router";
 import routerAdmin from "./routerAdmin";
 import { MORGAN_FORMAT } from "./libs/types/config";
 import morgan from "morgan";
-// frontend send data
+
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoStore = ConnectMongoDB(session);
+const store = new MongoStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "Sessions",
+});
 
 //1 - Entrance
 const app = express();
@@ -14,7 +22,19 @@ app.use(express.static(path.join(__dirname, "public"))); //middleware Dp > publi
 app.use(express.urlencoded({ extended: true })); //middleware Dp > tradional API support
 app.use(express.json()); //middleware DB > Rest IP support
 app.use(morgan(MORGAN_FORMAT)); //middleware Dp  > Loggig
+
 //2 - Session
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, //3 hours
+    },
+    resave: true,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
 
 //3 - Views
 app.set("views", path.join(__dirname, "views"));
